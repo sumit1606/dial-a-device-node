cc3502e243a5c9ed3c77750ecfd4dfae95fd99f6
Cominch <dominic@luetjohann.com>
2014-05-19 11:08:21 +0200

    add sim

---
+++ app/assets/javascripts/dial_a_device_node/devices/purebeaglebone_SIM.js
@@ -0,0 +1,13 @@
+(function(exports) {
+
+	var device_model_simulation = {
+
+
+    };
+
+    exports.init = function (eventbus) {
+ 
+	
+    };
+
+})(typeof exports == 'undefined'? this['simulator'] = {}: exports);
