537e3a503920220f74c12509dd4694125b6504e2
Cominch <dominic@luetjohann.com>
2014-06-03 11:58:47 +0200

    add weathercape UI

--- app/assets/javascripts/dial_a_device_node/devices/weathercape_SIM.js
+++ app/assets/javascripts/dial_a_device_node/devices/weathercape_SIM.js
@@ -2,10 +2,28 @@
 
     var device_model_simulation = {
 
+    	temperature: 25.3,
+        pressure: 1013.3,
+        humidity: 50,
+        lux: 200
 
     };
 
     exports.init = function (eventbus) {
+
+
+    	setInterval(function () {
+
+            eventbus.emit('ui.update', {
+                    "component": "all",
+                    "model": device_model_simulation
+                });
+
+
+        }, 1000);
+
+        eventbus.emit("serial.simulation");
+        eventbus.emit("serial.portopened");
 
 
     };
--- app/assets/javascripts/dial_a_device_node/devices/weathercape_UI.js
+++ app/assets/javascripts/dial_a_device_node/devices/weathercape_UI.js
@@ -18,12 +18,19 @@
 
             document.getElementById("temperature").innerHTML = data.model.temperature;
 
+            setTemperature(data.model.temperature);
+
             document.getElementById("pressure").innerHTML = data.model.pressure;
+
+            setPressure(data.model.pressure);
 
             document.getElementById("humidity").innerHTML = data.model.humidity;
 
+            setHumidity(data.model.humidity);
+
             document.getElementById("lux").innerHTML = data.model.lux;
 
+            setLux(data.model.lux);
 
         });
 
