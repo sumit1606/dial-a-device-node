4c9cbc3950d48ef88b200249fd8ca316eba218c5
Cominch <dominic@luetjohann.com>
2015-03-09 13:36:50 +0100

    fix vacuubrand simulation mode

--- app/assets/javascripts/dial_a_device_node/devices/vacuubrand_pc3001_SIM.js
+++ app/assets/javascripts/dial_a_device_node/devices/vacuubrand_pc3001_SIM.js
@@ -2,73 +2,31 @@
 
     var device_model_simulation = {
 
-        amount: 400,
-        time: 1000,
-        timecounter: 0,
-        amountcounter: 0,
-        flowrate: 400.0,
-        runmode: 0,
-        runfunction: 0,
-        cyclenumber: 1,
-        actualcyclenumber: 1
+        pump_engine: 0,
+        in_line_valve: 0,
+        coolant_valve: 0,
+        venting_valve: 0,
+        operation_mode: 1,
+
+        current_pressure: 1013.0,
+        current_speed: 0,
+        process_runtime: "0:0",
+        pressuresetpoint: 500,
+        speedsetpoint: 100,
+
+        language: 1,
+        pressureunit: 0,
+        autostart: 0,
+        acousticsignal: 0,
+
+        running: false
+
 
     };
 
     exports.init = function (eventbus) {
 
         setInterval(function () {
-
-
-            if (device_model_simulation.runfunction == 1) {
-
-                device_model_simulation.timecounter = device_model_simulation.timecounter + 1000;
-
-                
-                if (device_model_simulation.runmode == 0) {
-
-
-                    device_model_simulation.amountcounter = device_model_simulation.amountcounter + Math.round(device_model_simulation.flowrate / 60);
-
-                    if (device_model_simulation.amountcounter >= device_model_simulation.amount) {
-
-                        device_model_simulation.runfunction = 0;
-                    }
-
-                }
-
-                if (device_model_simulation.runmode == 1) {
-
-
-                    device_model_simulation.amountcounter = device_model_simulation.amountcounter + Math.round((device_model_simulation.amount / (device_model_simulation.time/1000)));
-
-                    if (device_model_simulation.timecounter >= device_model_simulation.time) {
-
-                        device_model_simulation.runfunction = 0;
-                    }
-
-                }
-
-                if (device_model_simulation.runmode == 2) {
-
-
-                    device_model_simulation.amountcounter = device_model_simulation.amountcounter + Math.round(device_model_simulation.flowrate / 60);
-
-                    if (device_model_simulation.timecounter >= device_model_simulation.time) {
-
-                        device_model_simulation.runfunction = 0;
-                    }
-
-                }
-
-            }
-
-            if (device_model_simulation.runfunction == 2) {
-
-                device_model_simulation.timecounter = 2000;                
-                device_model_simulation.runfunction = 0;
-                device_model_simulation.amountcounter = 175;
-
-            }
 
             eventbus.emit('ui.update', {
                     "component": "all",
@@ -83,43 +41,113 @@
         eventbus.emit("serial.simulation");
         eventbus.emit("serial.portopened");
 
+        setInterval(function () {
+
+            // simulation tick
+
+            if (device_model_simulation.venting_valve == 0) {
+
+                if ((device_model_simulation.operation_mode == 1) && (device_model_simulation.running)) {
+                    // Pump down
+                    var oldpressure = parseInt(device_model_simulation.current_pressure);
+                    var newpressure = oldpressure - 20;
+                    if (newpressure <= 0) {
+                        newpressure = 0;
+                    }
+                    device_model_simulation.current_pressure = newpressure.toString();
+                }
+
+                if ((device_model_simulation.operation_mode == 2) && (device_model_simulation.running)) {
+                    // Vac control
+                    var oldpressure = parseInt(device_model_simulation.current_pressure);
+
+                    var newpressure = oldpressure;
+
+                    if (oldpressure < device_model_simulation.pressuresetpoint) {
+                        newpressure = oldpressure + 20;
+                    }
+
+                    if (oldpressure > device_model_simulation.pressuresetpoint) {
+                        newpressure = oldpressure - 20;
+                    }
+
+                    if (Math.abs(oldpressure - newpressure) < 20) {
+                        newpressure = device_model_simulation.pressuresetpoint;
+                    }
+
+                    device_model_simulation.current_pressure = newpressure.toString();
+                }
+
+            } else {
+                // Increase pressure, because ventilation is open
+                var oldpressure = parseInt(device_model_simulation.current_pressure);
+                var newpressure = oldpressure + 300;
+                if (newpressure > 1013) {
+                    newpressure = 1013;
+                }
+                device_model_simulation.current_pressure = newpressure.toString();
+            }
+
+        }, 500);
+
+
         eventbus.on("device.command", function (data) {
 
-            if (data.command == "set_amount") {
 
-                device_model_simulation.amount = data.value;
+            if (data.command == "set_pressuresetpoint") {
 
-            }
 
-            if (data.command == "set_flowrate") {
-
-                device_model_simulation.flowrate = data.value;
+                device_model_simulation.pressuresetpoint = data.value;
 
             }
 
-            if (data.command == "set_time") {
+            if (data.command == "set_speedsetpoint") {
 
-                device_model_simulation.time = data.value;
+                device_model_simulation.speedsetpoint = data.value;
+
+            }
+
+            if (data.command == "set_pressureunit") {
+
+                device_model_simulation.pressureunit = data.value;
+            }
+
+
+            if (data.command == "set_start") {
+
+                device_model_simulation.running = true;
+
+            }
+
+            if (data.command == "set_stop") {
+
+                device_model_simulation.running = false;
+
+            }
+
+            if (data.command == "toggle_ventvalve") {
+
+                if (device_model_simulation.venting_valve == 0) {
+
+                    device_model_simulation.venting_valve = 1;
+
+                } else {
+
+                    device_model_simulation.venting_valve = 0;
+
+                }
+
+            }
+
+            if (data.command == "set_ventvalve") {
+
+                device_model_simulation.venting_valve = data.value;
 
             }
 
             if (data.command == "set_runmode") {
 
-                device_model_simulation.runmode = data.value;
-
-            }
-
-            if (data.command == "set_function") {
-
-                if (device_model_simulation.runfunction == 0 && data.value > 0) {
-
-                    device_model_simulation.timecounter = 0;
-                    device_model_simulation.amountcounter = 0;
-
-
-                }
-
-                device_model_simulation.runfunction = data.value;
+                device_model_simulation.operation_mode = data.value;
 
             }
 
--- lib/dial_a_device_node/version.rb
+++ lib/dial_a_device_node/version.rb
@@ -1,3 +1,3 @@
 module DialADeviceNode
-  VERSION = "0.0.154"
+  VERSION = "0.0.155"
 end
